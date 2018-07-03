24f016caf22b973bdbfeb43f85fdf33bbaf73e20
Jes Ababon <jesababon@gmail.com>
2018-07-02 22:15:27 -0400

    intial commit rendering all creatures

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+ Creature.create!({name: "Luke", description: "One-handed Jedi"})
+ Creature.create!({name: "Darth Vader", description: "Evil father of Luke."})
+ Creature.create!({name: "Cookie Monster", description: "Cookie-eating maniac"})
+ Creature.create!({name: "Swamp Thing", description: "Covered in moss"})
+ Creature.create!({name: "Alien", description: "Lizard thing with a mouth inside its mouth and acid for blood"})
+ Creature.create!({name: "Bigfoot", description: "Man-ape living in the forest.  Very big feet."})
+ Creature.create!({name: "Dracula", description: "Good dreser.  Vants to suck your blood."})
